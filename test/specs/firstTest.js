4e545b97c5a60d7c78c1506ac2c969a90474cb58
Maria Laura Estefania <maria.estefania@globant.com>
2025-02-25 17:29:03 -0300

    First file

---
+++ test/specs/firstTest.js
@@ -0,0 +1,15 @@
+describe('Ecommerce Application', () => {
+
+    it('Login Fail page', () => {
+
+        //webdriverio Async
+
+        browser.url("https://rahulshettyacademy.com/loginpagePractise/")
+
+        console.log("Titulo: ", browser.getTile())
+    })
+
+
+
+}
+)
