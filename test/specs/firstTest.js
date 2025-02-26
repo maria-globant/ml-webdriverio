133528f20ee3b29e0d6d14837aefe294a4130f1f
Maria Laura Estefania <maria.estefania@globant.com>
2025-02-26 15:24:21 -0300

    Dia 26/2 15:25

--- test/specs/firstTest.js
+++ test/specs/firstTest.js
@@ -1,3 +1,5 @@
+import { expect } from "expect-webdriverio"
+
 describe('Ecommerce Application', async () => {
 
     it('Login Fail page', async () => {
@@ -7,9 +9,18 @@ describe('Ecommerce Application', async () => {
         await browser.url("https://rahulshettyacademy.com/loginpagePractise/")
 
         console.log("Titulo: ", await browser.getTitle())
+        await expect(browser).toHaveTitle(expect.stringContaining("Rahul Shetty"))
     })
 
+    // Css selector - Xpath 
 
+    var person1 = await $("inputname[name='username']")
+    //await $("inputname[name='username']").setValue("rahulshettyacademy")
+    person1.setValue("rahulshettyacademy")
+
+    var person2 = $("#username")
+    //await $("#username").setValue("secondCSS")
+    person2.setValue("secondCSS")
 
 }
 )
