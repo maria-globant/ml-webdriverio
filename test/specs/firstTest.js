03d109539ff3d3b36cd069db0c04948fc112eea8
Maria Laura Estefania <maria.estefania@globant.com>
2025-02-27 12:27:49 -0300

    27/2 12:28

--- test/specs/firstTest.js
+++ test/specs/firstTest.js
@@ -6,23 +6,41 @@ describe('Ecommerce Application', async () => {
 
     it('Login Fail page', async () => {
 
-        //webdriverio Async
-
         await browser.url("https://rahulshettyacademy.com/loginpagePractise/")
-
         console.log("Titulo: ", await browser.getTitle())
+
         await expect(browser).toHaveTitle(expect.stringContaining("Rahul Shetty"))
+        await $("input[name='username']").setValue("rahulshettyacademy")
+        await browser.pause(2000)
+        await $("input[name='username']").setValue("Second")
+        const password = $("//input[@id='password']")
+        await password.setValue("learning")
+        await $("#signInBtn").click()
+        //await browser.pause(2000)
+        await console.log(await $(".alert-danger").getText())
 
-        // Css selector - Xpath 
+        await browser.waitUntil(async () => await $("#signInBtn").getAttribute('value') === 'Sign In',
+            {
+                timeout: 3000,
+                timeoutMsg: "Error message is not showing up"
+            })
+        await console.log(await $(".alert-danger").getText())
 
-        //await $("inputname[name='username']").setValue("rahulshettyacademy")
+        await expect($("p")).toHaveText(expect.stringContaining("username is"))
 
-        $("inputname[name='username']").setValue("rahulshettyacademy")
-
-        await browser.pause(3000)
     })
 
+    it('Login Success page', async () => {
 
+        await browser.url("https://rahulshettyacademy.com/loginpagePractise/")
+        await $("input[name='username']").setValue("rahulshettyacademy")
+        const password = $("//input[@id='password']")
+        await password.setValue("learning")
+        await $("#signInBtn").click()
 
-}
-)
+        await $(".btn-primary").waitForExist(2000)
+
+        await expect(browser).toHaveUrl(expect.stringContaining('shop'))
+        await expect(browser).toHaveTitle('ProtoCommerce')
+    })
+})
