9f1651bffe9740f47d7be109bb0fd9f2f6f37eaa
Maria Laura <maria.estefania@globant.com>
2025-10-21 12:07:58 -0300

    Lesson 46.1

--- test/specs/windowsFrames.js
+++ test/specs/windowsFrames.js
@@ -1,5 +1,7 @@
 import { expect as expectchai } from 'chai'
 
+// comando para correr:: npx wdio run wdio.conf.js 
+
 describe ('Windows and Frames Miscellanous', async() => 
 {
     it('Parent and Child windows swtich', async () =>
