d9c09d8ed7a8314aa8b73cfc68810f4902fe7237
mestefania-nex <maria.laura.estefania@ba.com>
2025-11-11 15:03:17 -0300

    Lesson 55.2

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -24,12 +24,15 @@ class LoginPage extends Page {
         return $(".alert-danger")
     }
 
+    get signIng () {
+        return $("#signInBtn")
+    }
 
     /**
      * a method to encapsule automation code to interact with the page
      * e.g. to login using username and password
      */
-    async login (username, password) {
+    async Login (username, password) {
         await this.inputUsername.setValue(username);
         await this.inputPassword.setValue(password);
         await this.btnSubmit.click();
---
+++ test/specs/poTest.js
@@ -0,0 +1,65 @@
+//import { expect } from "expect-webdriverio"
+
+import { expect, browser, $ } from '@wdio/globals'
+import LoginPage from '../pageobjects/login.page.js'
+
+
+describe('Ecommerce Application', async () => {
+
+    xit('Login Fail page', async () => {
+
+        await browser.url("https://rahulshettyacademy.com/loginpagePractise/")
+        console.log("Titulo: ", await browser.getTitle())
+        await expect(browser).toHaveTitle(expect.stringContaining("Rahul Shetty"))
+
+        let  loginPage = new LoginPage ()
+        loginPage.Login("rahulshettyacademy", "Second")
+
+        //await expect(browser).toHaveTitle(expect.stringContaining("Rahul Shetty"))
+        //await $("input[name='username']").setValue("rahulshettyacademy")
+        //await browser.pause(2000)
+        //await $("input[name='username']").setValue("Second")
+        //const password = $("//input[@id='password']")
+        //await password.setValue("learning")
+        //await $("#signInBtn").click()
+        //await browser.pause(2000)
+
+        //await loginPage.Login("rahulshettyacademy", "Second")
+
+        await console.log(await $(".alert-danger").getText())
+
+        await browser.waitUntil(async () => await $("#signInBtn").getAttribute('value') === 'Sign In',
+            {
+                timeout: 3000,
+                timeoutMsg: "Error message is not showing up"
+            })
+        await console.log(await $(".alert-danger").getText())
+
+        await expect($("p")).toHaveText(expect.stringContaining("username is"))
+
+    })
+
+    it('Login Success page', async () => {
+
+        await browser.url("https://rahulshettyacademy.com/loginpagePractise/")
+     //   await $("input[name='username']").setValue("rahulshettyacademy")
+     //   const password = $("//input[@id='password']")
+     //   await password.setValue("learning")
+
+        let  loginPage = new LoginPage ()
+
+        await loginPage.login("rahulshettyacademy", "learning")
+
+        await console.log ("Alert ------------", await loginPage.alert.getText())
+
+        /**** 
+        await $("#signInBtn").click()
+
+        await $(".btn-primary").waitForExist(2000)
+
+        await expect(browser).toHaveUrl(expect.stringContaining('shop'))
+        await expect(browser).toHaveTitle('ProtoCommerce')
+        ***/
+    })
+
+})
